9d0ee4c8ea641fe84083616284326500428eb988
Yulia Belyakova <yuliya.beliakova@gmail.com>
2020-10-22 15:50:29 +0200

    small format edit

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,14 +3,17 @@ import dotenv from 'dotenv';
 
 dotenv.config({path: './.env'})
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, 
+const sequelize = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASS, 
   {
     host: process.env.DB_HOST,
     dialect: 'mysql'
   });
 
 sequelize.authenticate()
-  .then(()=> console.log('Database connected'))
+  .then(()     => console.log('Database connected'))
   .catch((err) => console.log(`Error: ${err}`));
 
 export {sequelize};
