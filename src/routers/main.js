b19048dba4e32320dc484c9e87fee55a418d85c1
Yulia Belyakova <yuliya.beliakova@gmail.com>
2020-10-24 23:41:20 +0200

    Add wallet endpoint

--- src/routers/main.js
+++ src/routers/main.js
@@ -2,6 +2,7 @@ import express from 'express';
 import authMiddleware from '../middlewares/auth.js';
 import Stock from '../models/stock.js';
 import User from '../models/user.js';
+import Wallet from '../models/wallet.js';
 import Transaction from '../models/transaction.js';
 
 const router = express.Router();
@@ -23,6 +24,29 @@ router.get('/transactions', async (req, res) => {
     res.json(result);
   } catch(error) {
     res.status(404).json({ data: null, error: 'Unknown error' });
+  } 
+});
+
+router.get('/wallet', async (req, res) => {
+  try {
+    const id = req.user.id;
+    let wallet = [];
+    let valueOfAssets = 0;
+    const walletPositions = await Wallet.findAll({where: {user_id: id}, include: [{model: Stock, as: 'stock'}]})
+
+    for (const position of walletPositions) {
+      const coin = await Stock.findOne({where: {stock_id: position.stock_id}})
+      wallet.push({coin: position.stock.symbol, quantity: position.quantity, price: coin.dataValues.price, value: position.quantity * coin.dataValues.price})
+      valueOfAssets += position.quantity * coin.dataValues.price
+    }
+
+    const balance = await User.findOne({where: {user_id: id}, attributes: ['balance']});
+    wallet.push({balance: balance.dataValues.balance + valueOfAssets});
+
+    res.json(wallet);
+
+  } catch(error) {
+    console.log(error);
   } 
 });
 
