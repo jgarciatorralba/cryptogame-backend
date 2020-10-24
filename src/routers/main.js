7bbe94175a997e78a24f26ff0d8acaec78390944
Christian Callau <ch.callau@gmail.com>
2020-10-25 00:48:02 +0200

    Add minor fixes

--- src/routers/main.js
+++ src/routers/main.js
@@ -95,22 +95,22 @@ router.get('/wallet', async (req, res) => {
     const id = req.user.id;
     let wallet = [];
     let valueOfAssets = 0;
-    const walletPositions = await Wallet.findAll({where: {user_id: id}, include: [{model: Stock, as: 'stock'}]})
+    const walletPositions = await Wallet.findAll({ where: { user_id: id }, include: [{ model: Stock, as: 'stock' }] });
 
     for (const position of walletPositions) {
-      const coin = await Stock.findOne({where: {stock_id: position.stock_id}})
-      wallet.push({coin: position.stock.symbol, quantity: position.quantity, price: coin.dataValues.price, value: position.quantity * coin.dataValues.price})
-      valueOfAssets += position.quantity * coin.dataValues.price
+      const coin = await Stock.findOne({ where: { stock_id: position.stock_id } });
+      wallet.push({ coin: position.stock.symbol, quantity: position.quantity, price: coin.dataValues.price, value: position.quantity * coin.dataValues.price });
+      valueOfAssets += position.quantity * coin.dataValues.price;
     }
 
-    const balance = await User.findOne({where: {user_id: id}, attributes: ['balance']});
-    wallet.push({balance: balance.dataValues.balance + valueOfAssets});
+    const balance = await User.findOne({ where: { user_id: id }, attributes: ['balance'] });
+    wallet.push({ balance: balance.dataValues.balance + valueOfAssets });
 
     res.json(wallet);
 
-  } catch(error) {
+  } catch (error) {
     console.log(error);
-  } 
+  }
 });
 
 export default router;
