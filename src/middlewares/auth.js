4cfa09b0ce30c9d6301f4ad34ba07bb58716e857
Christian Callau <ch.callau@gmail.com>
2020-10-25 00:17:47 +0200

    Refactor auth login

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,10 +4,20 @@ import config from '../config/app-config.js';
 export default function (req, res, next) {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
-  if (token == null) return res.sendStatus(401);
+
+  if (token == null) {
+    return res
+      .status(401)
+      .json({ data: null, error: 'Unauthorized: Missing JWT' });
+  }
 
   jwt.verify(token, config.app.accessTokenSecret, (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err) {
+      return res
+        .status(403)
+        .json({ data: null, error: 'Forbidden: Invalid JWT' });
+    }
+
     req.user = user;
     next();
   });
