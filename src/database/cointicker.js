9a43fff0ebe4e75560388e3ea2cb8b8236aa172c
Christian Callau <ch.callau@gmail.com>
2020-10-24 03:09:43 +0200

    Add coin ticker columns

--- src/database/cointicker.js
+++ src/database/cointicker.js
@@ -1,14 +1,22 @@
 import WebSocket from 'ws';
+import Stock from '../models/stock.js';
 
 export default {
-  watch: function () {
-    const symbols = ['BTCUSDT'];
+  watch: async function () {
+    const symbols = (await Stock.findAll()).map(s => s.symbol);
     const ws = new WebSocket('wss://stream.binance.com:9443/ws/!ticker@arr');
     ws.on('message', function incoming(response) {
       const data = JSON.parse(response);
       for (const coin of data) {
         if (symbols.includes(coin.s)) {
-          console.log(`${coin.s}: ${coin.b} $`);
+          Stock.update({
+            price: coin.c,
+            change: coin.P,
+            high: coin.h,
+            low: coin.l,
+            volume: coin.v,
+            trades: coin.n
+          }, { where: { symbol: coin.s } });
         }
       }
     });
