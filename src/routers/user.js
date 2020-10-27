ef6067ab81c2b98708f2ae4ab3fcb3f176278dd1
Yulia Belyakova <yuliya.beliakova@gmail.com>
2020-10-27 17:04:45 +0100

    Add encryption of password update in user update endpoint

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
 import authMiddleware from '../middlewares/auth.js';
 import User from '../models/user.js';
+import config from '../config/app-config.js';
 
 const router = express.Router();
 
@@ -20,7 +22,7 @@ router.delete('/', async (req, res) => {
 
 router.patch('/', async (req, res) => {
   const userId = req.user.id;
-  const userUpdate = req.body;
+  const userUpdate = {email: req.body.email, password: await bcrypt.hash(req.body.password, config.app.saltRounds), name: req.body.name };
   await User.update(userUpdate, { where: {user_id: userId} });
   res.json({ data: "User updated!", error: null });
 });
