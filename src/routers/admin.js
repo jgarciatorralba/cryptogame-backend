94d6ec9cc550179c4acb0b37b053244cdd27f4bb
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-29 11:24:01 +0100

    Edit 'elseif' in 'admin.js' for coin restore and add

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import authMiddleware from '../middlewares/auth.js';
-import adminMiddleware from '../middlewares/admin.js';
-import Stock from '../models/stock.js';
-import User from '../models/user.js';
+import express from "express";
+import authMiddleware from "../middlewares/auth.js";
+import adminMiddleware from "../middlewares/admin.js";
+import Stock from "../models/stock.js";
+import User from "../models/user.js";
 
 const router = express.Router();
 
@@ -10,64 +10,76 @@ router.use(authMiddleware);
 router.use(adminMiddleware);
 
 // Coin CRuD
-router.post('/coin', async (req, res) => {
+router.post("/coin", async (req, res) => {
   const coin = req.body;
-  
-  const checkCoin = await Stock.findOne({ where: { name: coin.name}, paranoid: false});
 
-  if(checkCoin && checkCoin.destroyTime == null) {
+  const checkCoin = await Stock.findOne({
+    where: { name: coin.name },
+    paranoid: false,
+  });
+
+  if (checkCoin && checkCoin.destroyTime == null) {
     res.json({ data: null, error: "Coin already exists!" });
-  } else if (checkCoin.destroyTime) {
-    await Stock.restore({where: {'stock_id': checkCoin.stock_id}});
+  } else if (checkCoin && checkCoin.destroyTime) {
+    await Stock.restore({ where: { stock_id: checkCoin.stock_id } });
     res.json({ data: "Coin restored!", error: null });
   } else {
-     await Stock.create(coin);
-  res.json({ data: "Coin added!", error: null });
+    await Stock.create(coin);
+    res.json({ data: "Coin added!", error: null });
   }
 });
 
-router.get('/coin/:coinId', async (req, res) => {
+router.get("/coin/:coinId", async (req, res) => {
   const coinId = req.params.coinId;
   const coin = await Stock.findOne({ where: { stock_id: coinId } });
-  res.json({ data: { symbol: coin.symbol, pair: coin.pair, name: coin.name }, error: null });
+  res.json({
+    data: { symbol: coin.symbol, pair: coin.pair, name: coin.name },
+    error: null,
+  });
 });
 
-router.delete('/coin/:coinId', async (req, res) => {
+router.delete("/coin/:coinId", async (req, res) => {
   const coinId = req.params.coinId;
-  await Stock.destroy({ where: { stock_id: coinId }});
+  await Stock.destroy({ where: { stock_id: coinId } });
   res.json({ data: "Coin soft deleted!", error: null });
 });
 
 // User cRUD
-router.get('/users/:page&:limit', async (req, res) => {
+router.get("/users/:page&:limit", async (req, res) => {
   const page = parseInt(req.params.page);
   const limit = parseInt(req.params.limit);
   const offset = (page - 1) * limit;
-  const users = await User.findAndCountAll({ attributes: ['user_id', 'email', 'name', 'avatar', 'role', 'balance'], limit: limit, offset: offset });
+  const users = await User.findAndCountAll({
+    attributes: ["user_id", "email", "name", "avatar", "role", "balance"],
+    limit: limit,
+    offset: offset,
+  });
 
-  res.json(
-    {
-      data: users.rows,
-      currentPage: page,
-      totalPages: Math.ceil(users.count / limit),
-      error: null
-    });
+  res.json({
+    data: users.rows,
+    currentPage: page,
+    totalPages: Math.ceil(users.count / limit),
+    error: null,
+  });
 });
 
-router.get('/user/:userId', async (req, res) => {
+router.get("/user/:userId", async (req, res) => {
   const userId = req.params.userId;
-  const user = await User.findOne({ where: { user_id: userId }, attributes: ['user_id', 'email', 'name', 'avatar', 'role', 'balance'] });
+  const user = await User.findOne({
+    where: { user_id: userId },
+    attributes: ["user_id", "email", "name", "avatar", "role", "balance"],
+  });
   res.json({ data: user, error: null });
 });
 
-router.patch('/user/:userId', async (req, res) => {
+router.patch("/user/:userId", async (req, res) => {
   const userId = req.params.userId;
   const userUpdate = req.body;
   await User.update(userUpdate, { where: { user_id: userId } });
   res.json({ data: "User updated!", error: null });
 });
 
-router.delete('/user/:userId', async (req, res) => {
+router.delete("/user/:userId", async (req, res) => {
   const userId = req.params.userId;
   await User.destroy({ where: { user_id: userId } });
   res.json({ data: "User soft deleted!", error: null });
